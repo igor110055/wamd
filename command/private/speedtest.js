e93af55386461e12301a661590e7b9d702f24b65
xbnfz01 <88994722+xbnfz01@users.noreply.github.com>
2022-06-04 01:13:24 +0700

    Add files via upload

--- command/private/speedtest.js
+++ command/private/speedtest.js
@@ -1,49 +1,49 @@
-const speedTest = require('speedtest-net');
+// const speedTest = require('speedtest-net');
 
-module.exports = {
-    name: 'speedtest',
-    description: 'Test your internet speed',
-    owner: true,
-    async exec({
-        msg,
-        args,
-        sock
-    }) {
-        try {
-            await msg.reply("Testing your internet speed...");
-            var st = await speedTest({
-                acceptLicense: true,
-                acceptGdpr: true,
-            });
-            let capt = `📡Ｓｐｅｅｄｔｅｓｔ - Ｓｅｒｖｅｒ
+// module.exports = {
+//     name: 'speedtest',
+//     description: 'Test your internet speed',
+//     owner: true,
+//     async exec({
+//         msg,
+//         args,
+//         sock
+//     }) {
+//         try {
+//             await msg.reply("Testing your internet speed...");
+//             var st = await speedTest({
+//                 acceptLicense: true,
+//                 acceptGdpr: true,
+//             });
+//             let capt = `📡Ｓｐｅｅｄｔｅｓｔ - Ｓｅｒｖｅｒ
             
-» *Time     :* ${st.timestamp}
-» *Download :* ${speedText(st.download.bandwidth)}
-» *Upload   :* ${speedText(st.upload.bandwidth)}
-» *Isp      :* ${st.isp}
-» *Ping     :* ${st.ping.latency} ms
+// » *Time     :* ${st.timestamp}
+// » *Download :* ${speedText(st.download.bandwidth)}
+// » *Upload   :* ${speedText(st.upload.bandwidth)}
+// » *Isp      :* ${st.isp}
+// » *Ping     :* ${st.ping.latency} ms
 
-[Result] : (${st.result.url})`;
+// [Result] : (${st.result.url})`;
 
-            await msg.reply(capt);
+//             await msg.reply(capt);
 
-        } catch (err) {
-            console.log(err);
-            await msg.reply("Gagal mengecek internet speed");
-        }
-    }
-}
+//         } catch (err) {
+//             console.log(err);
+//             await msg.reply("Gagal mengecek internet speed");
+//         }
+//     }
+// }
 
 
-function speedText(speed) {
-    let bits = speed * 8;
-    const units = ["", "K", "M", "G", "T"];
-    const places = [0, 1, 2, 3, 3];
-    let unit = 0;
-    while (bits >= 2000 && unit < 4) {
-        unit++;
-        bits /= 1000;
-    }
+// function speedText(speed) {
+//     let bits = speed * 8;
+//     const units = ["", "K", "M", "G", "T"];
+//     const places = [0, 1, 2, 3, 3];
+//     let unit = 0;
+//     while (bits >= 2000 && unit < 4) {
+//         unit++;
+//         bits /= 1000;
+//     }
 
-    return `${bits.toFixed(places[unit])} ${units[unit]}bps`;
-}
+//     return `${bits.toFixed(places[unit])} ${units[unit]}bps`;
+// }
